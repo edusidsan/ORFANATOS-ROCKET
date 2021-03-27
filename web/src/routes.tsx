d08989b72b86e779c7c5b4896c5d873a75ef75df
Eduardo Santos <edu.sidnei@hotmail.com>
2021-03-27 18:43:33 -0300

    app

---
+++ web/src/routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// import CreateOrphanage from './pages/CreateOrphanage';
+import Landing from './pages/Landing/index';
+// import Orphanage from './pages/Orphanage';
+import OrphanagesMap from './pages/OrphanagesMap';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Landing} />  {/* EXACT PARA NAO DAR CONFLITO COM A BARRA, SENAO COLOCA UMA PAGINA SOBRE A OUTRA*/}
+        <Route path="/app" component={OrphanagesMap} />
+        {/* <Route path="/orphanages/create" component={CreateOrphanage} /> */}
+        {/* <Route path="/orphanages/:id" component={Orphanage} /> */}
+      </Switch>
+    </BrowserRouter>
+  );
+}
